07d90607e2902fcb431c270d35752d2961776ea7
0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>
2024-04-02 07:09:19 +0000

    precompile_add_fix
    Signed-off-by: 0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>

--- src/succinct.rs
+++ src/succinct.rs
@@ -3,6 +3,6 @@ extern "C" {
 }
 
 #[inline]
-pub fn bls12381_add(p: &mut [u8; 32], q: &[u8; 32]) {
+pub fn bls12381_add(p: &mut [u8; 96], q: &[u8; 96]) {
     unsafe { syscall_bls12381_add(p.as_mut_ptr() as *mut u32, q.as_ptr() as *const u32) }
 }
